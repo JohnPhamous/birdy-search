67f71b7c1cdc0df5d5ffb5bd9e51c207a1372f4a
John Pham <johnphammail@gmail.com>
2018-04-03 10:37:51 -0700

    adding geo filter and all stream functions

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ let Twit = require('twit')
 let twitterAuth = require('./config/twitterAuth')
 
 let T = new Twit(twitterAuth)
+let dataBuffer = []
+
+let grabLinkData = (link) => {
+    console.log('do link stuff')
+}
+
 
 let sanFrancisco = [ '-122.75', '36.8', '-121.75', '37.8' ]
-
 let stream = T.stream('statuses/filter', { locations: sanFrancisco })
-
 stream.on('tweet', function (tweet) {
-  console.log(tweet)
+    console.log(tweet)
+    console.log(typeof tweet.user.description)
+    if (tweet.user.description.includes('http')) {
+        grabLinkData(tweet.user.description)
+    }
 })
+
+//let stream = T.stream('statuses/sample')
+
+//stream.on('tweet', function (tweet) {
+  //console.log(tweet)
+//})
