a3cbceb4bf88006449d963afaf6c37d3a95ffe7e
William Shiao <willshiao@gmail.com>
2018-04-03 20:46:59 -0700

    Make script fetch website urls
    Also make the script write the output to a file.

--- index.js
+++ index.js
@@ -1,27 +1,72 @@
-let fs = require('fs')
-let Twit = require('twit')
-let twitterAuth = require('./config/twitterAuth')
+const fs = require('fs')
+const path = require('path')
+const Twit = require('twit')
+const config = require('config')
+const rp = require('request-promise')
+const cheerio = require('cheerio')
+const { fork } = require('child_process')
 
-let T = new Twit(twitterAuth)
-let dataBuffer = []
+const T = new Twit(config.get('twitter'))
+const tweetProcessor = fork(`${__dirname}/lib/tweetProcessor.js`)
 
-let grabLinkData = (link) => {
-    console.log('do link stuff')
+const linkRegex = /https?:\/\/[\-a-zA-Z\.?0-9@:%._\+~#=\/&]+/
+let fileNumber = 0
+let outputFile = fs.createWriteStream(getOutputFilename())
+const pendingQueue = []
+let pendingRequests = 0
+let writeSize = 0
+
+function getOutputFilename() {
+  return path.join(config.get('dataDir'), `tweets-${fileNumber}.json`)
 }
 
+async function grabLinkData (tweet) {
+  if (pendingRequests >= config.get('requestLimit')) {
+    pendingQueue.push(tweet)
+    return
+  }
+  const link = linkRegex.exec(tweet.text)[0]
+  console.log('Got link:', link)
+  const res = await rp.get(link)
+  const $ = cheerio.load(res)
+  const title = $('title').text()
+  console.log('Got title: ', title)
+  tweet.title = title
+  writeToFile(tweet)
+  pendingRequests--
+  if(pendingQueue.length > 0) {
+    grabLinkData(pendingQueue.pop())
+  }
+}
 
-let sanFrancisco = [ '-122.75', '36.8', '-121.75', '37.8' ]
-let stream = T.stream('statuses/filter', { locations: sanFrancisco })
-stream.on('tweet', function (tweet) {
-    console.log(tweet)
-    console.log(typeof tweet.user.description)
-    if (tweet.user.description.includes('http')) {
-        grabLinkData(tweet.user.description)
-    }
+function writeToFile(tweet) {
+  const tweetString = JSON.stringify(tweet) + '\n'
+  writeSize += tweetString.length
+  outputFile.write(tweetString)
+  console.log('Output buffer size:', writeSize)
+  if(writeSize > 10000000) {
+    writeSize = 0
+    outputFile.close()
+    fileNumber++
+    outputFile = fs.createWriteStream(getOutputFilename())
+  }
+}
+
+const sanFrancisco = [ '-122.75', '36.8', '-121.75', '37.8' ]
+
+const stream = T.stream('statuses/filter', { locations: sanFrancisco, tweet_mode: 'extended' })
+console.log('Starting Twitter Streaming API')
+
+stream.on('tweet', (tweet) => {
+  if (linkRegex.test(tweet.text)) {
+    grabLinkData(tweet)
+  } else {
+    writeToFile(tweet)
+  }
 })
 
-//let stream = T.stream('statuses/sample')
+// let stream = T.stream('statuses/sample')
 
-//stream.on('tweet', function (tweet) {
-  //console.log(tweet)
-//})
+// stream.on('tweet', function (tweet) {
+//   console.log(tweet)
+// })
