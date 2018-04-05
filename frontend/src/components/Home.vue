309b047d3895c9a86f84c9e10c4dbf40974e5bd0
John Pham <johnphammail@gmail.com>
2018-04-05 09:34:15 -0700

    search init

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,11 +1,24 @@
 <template>
-  
+  <search-bar />
 </template>
 
 <script>
-export default {}
+import SearchBar from './SearchBar'
+export default {
+    components: {
+        SearchBar
+    }
+}
 </script>
 
 <style>
-
+html,
+body {
+    background: rgb(29, 35, 56);
+    background: linear-gradient(
+        45deg,
+        rgba(77, 94, 144, 1) 0% rgba(29, 35, 56, 1) 100%
+    );
+    color: white;
+}
 </style>
--- frontend/src/components/SearchBar.vue
+++ frontend/src/components/SearchBar.vue
@@ -1,11 +1,19 @@
 <template>
-  
+  <div class="search-bar">
+    <h1>Birdy Search</h1>
+    <input type="text">
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style>
-
+<style scoped>
+.search-bar {
+    text-align: center;
+}
+.search-bar input {
+    width: 80%;
+}
 </style>
