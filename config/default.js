028c026fa5791b778a406325c865a02c2fb4104a
William Shiao <willshiao@gmail.com>
2018-04-11 12:06:58 -0700

    Print stack track on warnings

--- config/default.js
+++ config/default.js
@@ -1,17 +1,17 @@
 module.exports = {
-    twitter: {
-        consumer_key: '',
-        consumer_secret: '',
-        access_token: '',
-        access_token_secret: ''
-    },
-    dataDir: 'data',
-    requestLimit: 10, // Max number of concurrent requests
+  twitter: {
+    consumer_key: '',
+    consumer_secret: '',
+    access_token: '',
+    access_token_secret: ''
+  },
+  dataDir: 'data',
+  requestLimit: 10, // Max number of concurrent requests
 
-    // Max number of characters to store in a single file. 10000000 characters ~= 10 MB
-    fileSizeLimit: 10000000,
-    // Max number of characters to store overall. 10000000 * 750 character ~= 7.5 GB
-    totalSizeLimit: 10000000 * 750,
-    numWorkers: 3,
-    linkRegex: /https?:\/\/[-a-zA-Z.?0-9@:%._+~#=/&]+/ // A regex used to find links
+  // Max number of characters to store in a single file. 10000000 characters ~= 10 MB
+  fileSizeLimit: 10000000,
+  // Max number of characters to store overall. 10000000 * 750 character ~= 7.5 GB
+  totalSizeLimit: 10000000 * 750,
+  numWorkers: 3,
+  linkRegex: /https?:\/\/[-a-zA-Z.?0-9@:%._+~#=/&]+/ // A regex used to find links
 }
--- index.js
+++ index.js
@@ -12,11 +12,11 @@ let currentWorker = 0
 
 // creates numWorkers of workers
 for (let i = 0; i < numWorkers; i++) {
-    workers.push(fork('./lib/urlWorker.js'))
-    // once worker grabs the link metadata, write to file
-    workers[i].on('message', processedTweet => {
-        writeToFile(processedTweet)
-    })
+  workers.push(fork('./lib/urlWorker.js'))
+  // once worker grabs the link metadata, write to file
+  workers[i].on('message', processedTweet => {
+    writeToFile(processedTweet)
+  })
 }
 
 // Instantiate the Twit object
@@ -30,16 +30,16 @@ const linkRegex = config.get('linkRegex')
 
 // Find the last file number to make sure we don't accidentally overwrite previously saved data
 try {
-    while (fs.statSync(getOutputFilename())) {
-        // If an ENOENT error is not thrown, the file exists
-        fileNumber++ // Check the next file
-    }
+  while (fs.statSync(getOutputFilename())) {
+    // If an ENOENT error is not thrown, the file exists
+    fileNumber++ // Check the next file
+  }
 } catch (err) {
-    if (err.code === 'ENOENT') {
-        console.log('Set output file to', getOutputFilename())
-    } else {
-        debug('Unknown error:', err)
-    }
+  if (err.code === 'ENOENT') {
+    console.log('Set output file to', getOutputFilename())
+  } else {
+    debug('Unknown error:', err)
+  }
 }
 
 // Open a write stream to the output file
@@ -49,8 +49,8 @@ let outputFile = fs.createWriteStream(getOutputFilename())
  * Convinience function to return the path to the output file
  * @return   {String}  The output file path
  */
-function getOutputFilename() {
-    return path.join(config.get('dataDir'), `tweets-${fileNumber}.json`)
+function getOutputFilename () {
+  return path.join(config.get('dataDir'), `tweets-${fileNumber}.json`)
 }
 
 /**
@@ -58,48 +58,50 @@ function getOutputFilename() {
  *  Also rotates files if the current file is too large.
  * @param   {Twit.Tweet}  tweet  The tweet to write to a file
  */
-function writeToFile(tweet) {
-    // Convert the tweet to JSON
-    const tweetString = JSON.stringify(tweet) + '\n'
-    writeSize += tweetString.length
-    totalSize += tweetString.length
-    outputFile.write(tweetString)
-    debug('Output buffer size:', writeSize)
+function writeToFile (tweet) {
+  // Convert the tweet to JSON
+  const tweetString = JSON.stringify(tweet) + '\n'
+  writeSize += tweetString.length
+  totalSize += tweetString.length
+  outputFile.write(tweetString)
+  debug('Output buffer size:', writeSize)
 
-    // Check if the number of characters exceeds the file character size limit
-    //  which, by default, is equal to ~10 MB of text
-    if (
-        writeSize > config.get('fileSizeLimit') ||
+  // Check if the number of characters exceeds the file character size limit
+  //  which, by default, is equal to ~10 MB of text
+  if (
+    writeSize > config.get('fileSizeLimit') ||
         totalSize >= config.get('totalSizeLimit')
-    ) {
-        writeSize = 0
-        outputFile.close()
-        // Rotate file streams to the next one
-        fileNumber++
-        outputFile = fs.createWriteStream(getOutputFilename())
-        if (totalSize >= config.get('totalSizeLimit')) {
-            console.log('Reached the total size limit. Exiting...')
-            process.exit(0)
-        }
+  ) {
+    writeSize = 0
+    outputFile.close()
+    // Rotate file streams to the next one
+    fileNumber++
+    outputFile = fs.createWriteStream(getOutputFilename())
+    if (totalSize >= config.get('totalSizeLimit')) {
+      console.log('Reached the total size limit. Exiting...')
+      process.exit(0)
     }
+  }
 }
 
 // Creating a bounding box for the world
 // We want to gather all geotagged tweets
 const world = ['-180', '-90', '180', '90']
-//const world = [ '-122.75', '36.8', '-121.75', '37.8' ]
+// const world = [ '-122.75', '36.8', '-121.75', '37.8' ]
 // Create the tweet stream
 const tweetStream = T.stream('statuses/filter', { locations: world })
 console.log('Starting Twitter Streaming API')
 
 tweetStream.on('tweet', tweet => {
-    // On a tweet, test if it contains a link
-    if (linkRegex.test(tweet.text)) {
-        // If it contains a link, use grabLinkData() to process it
-        workers[currentWorker].send(tweet)
-        currentWorker = (currentWorker + 1) % numWorkers
-    } else {
-        // Otherwise, write it directly to a file
-        writeToFile(tweet)
-    }
+  // On a tweet, test if it contains a link
+  if (linkRegex.test(tweet.text)) {
+    // If it contains a link, use grabLinkData() to process it
+    workers[currentWorker].send(tweet)
+    currentWorker = (currentWorker + 1) % numWorkers
+  } else {
+    // Otherwise, write it directly to a file
+    writeToFile(tweet)
+  }
 })
+
+process.on('warning', e => console.warn(e.stack))
