d7e2abba0530c0971ea0c86cec7bd6d2f690851c
John Pham <johnphammail@gmail.com>
2018-04-12 09:59:43 -0700

    updating home page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,16 +8,24 @@
 import Home from './components/Home'
 
 export default {
-    name: 'App',
-    components: {
-        Home
-    }
+  name: 'App',
+  components: {
+    Home
+  }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Istok+Web');
+@import url('https://fonts.googleapis.com/css?family=Lora');
+* {
+  font-family: 'Istok Web', sans-serif;
+}
+p {
+  font-family: 'Lora', serif;
+}
 #app {
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 }
 </style>
--- frontend/src/components/SearchBar.vue
+++ frontend/src/components/SearchBar.vue
@@ -3,50 +3,90 @@
     <div class="card">
       <div class="card-content">
         <h1>Birdy Search</h1>
-        <input type="text" placeholder="Twitter Search Query" />
-        <button>Look Up</button>
+        <form v-on:submit.prevent="submitQuery">
+          <input type="text" v-model="query" placeholder="Twitter Search Query" />
+          <button class="btn">Look Up</button>
+        </form>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+import ResultList from './ResultList'
+
+export default {
+  data: function() {
+    return {
+      query: ''
+    }
+  },
+  methods: {
+    submitQuery: function() {
+      console.log('Submitting Query', this.query)
+    }
+  },
+  components: {
+    ResultList
+  }
+}
 </script>
 
 <style scoped>
 .card {
-    background: linear-gradient(45deg, #1e2339 0%, #4f6093 100%);
-    height: 90vh;
-    width: 95vw;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    border-radius: 20px;
+  background: linear-gradient(45deg, #1e2339 0%, #4f6093 100%);
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  padding: 50px 100px;
+  align-items: center;
 }
 .search-bar {
-    text-align: center;
-    display: flex;
-    height: 100%;
-    align-items: center;
+  text-align: center;
+  display: flex;
+  height: 100%;
+  align-items: center;
 }
 .search-bar input[type='text'] {
-    width: 50%;
-    background: white;
-    border: none;
-    border-radius: 10px;
-    height: 50px;
-    line-height: 20px;
-    font-size: 20px;
-    padding: 10px 20px;
-    font-weight: 800;
+  width: 50vw;
+  background: white;
+  border: none;
+  border-radius: 10px;
+  height: 30px;
+  line-height: 20px;
+  font-size: 20px;
+  padding: 10px 20px;
+  font-weight: 800;
 }
 .search-bar button {
-    height: 50px;
-    font-size: 20px;
-    font-weight: 800px;
-    border-radius: 10px;
-    padding: 10px 20px;
-    border: none;
+  height: 50px;
+  font-size: 20px;
+  font-weight: 800;
+  border-radius: 10px;
+  padding: 10px 20px;
+  border: none;
+  cursor: pointer;
+}
+.btn {
+  flex: 1 1 auto;
+  margin: 10px;
+  padding: 30px;
+  text-align: center;
+  text-transform: uppercase;
+  transition: 0.5s;
+  background-size: 200% auto;
+  color: white;
+  background-image: linear-gradient(
+    to right,
+    rgb(133, 138, 243) 0%,
+    rgb(193, 73, 236) 51%,
+    rgb(238, 96, 190) 100%
+  );
+}
+.btn:hover {
+  background-position: right center; /* change the direction of the change here */
 }
 </style>
